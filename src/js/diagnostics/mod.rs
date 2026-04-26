646396cc24c80cfa3a66d8f3d708ba9a4676607c
Eran Boodnero <eboodnero@gmail.com>
2026-04-26 08:17:46 -0700

    Restore shared helpers in diagnostics modules

--- src/js/diagnostics/mod.rs
+++ src/js/diagnostics/mod.rs
@@ -2,5 +2,15 @@
 mod input;
 mod script;
 
+use proc_macro2::Span;
+use syn::Error;
+
 pub(crate) use input::*;
 pub(crate) use script::*;
+
+fn error(span: Span, summary: impl AsRef<str>, help: impl AsRef<str>) -> Error {
+    Error::new(
+        span,
+        format!("{}\nhelp: {}", summary.as_ref(), help.as_ref()),
+    )
+}
